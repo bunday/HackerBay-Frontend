fda04dc585f9cb54f10f12c3865d53575cd57110
Zadat Olayinka <bundayy@outlook.com>
2020-05-05 17:24:04 +0100

    class based component + set grid size

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  // State Declaraction
+  state = {
+    rows: 0,
+    columns: 0,
+    gridSize: 0
+  }
+  componentWillMount() {
+    this.getBasicGridSize()
+  }
+
+  // Set Grid Size
+  getBasicGridSize() {
+    let columns = parseInt(prompt('Enter number of desired columns between 2 and 25'));
+    columns = isNaN(columns) ? 25 : (columns > 25 ? 25 : (columns < 2 ? 2 : columns));
+    
+    let rows = parseInt(prompt('Enter number of desired rows between 2 and 25'));
+    rows = isNaN(rows) ? 25 : (rows > 25 ? 25 : (rows < 2 ? 2 : rows));
+    
+    const gridSize = rows * columns;
+
+    this.setState({
+      rows, columns, gridSize
+    })
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+        <h2> HackerBay Frontend Test by Zadat Olayinka</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
